4ff65d2cfbdf59ebeae1c4fb4a4daf16b9ea6724
Philipp Oberdiek <git@oberdiek.net>
2023-03-03 11:44:45 +0100

    Rebuild GradientMetricCollector
    - Now using `torch.autograd.grad()` instead of `loss.backward()`.
    - Freeing graph in laster grad call to avoid memory leaks.
    - Adjust the collector to the changed interface of `GradientMetric`
      instances

--- gradient_metrics/general.py
+++ gradient_metrics/general.py
@@ -1,7 +1,7 @@
-from typing import List, Sequence, Union
+from collections import defaultdict
+from typing import Dict, List, Union
 
 import torch
-import torch.nn as nn
 
 from gradient_metrics.metrics import GradientMetric
 
@@ -9,7 +9,7 @@ from gradient_metrics.metrics import GradientMetric
 class GradientMetricCollector(object):
     def __init__(
         self,
-        metrics: Union[Sequence[GradientMetric], GradientMetric],
+        metrics: Union[List[GradientMetric], GradientMetric],
     ) -> None:
         """Helper class for computing gradients.
 
@@ -21,19 +21,37 @@ class GradientMetricCollector(object):
             ValueError: If the list of metrics is empty.
         """
 
-        self.metrics = (
-            tuple(metrics) if isinstance(metrics, (list, tuple)) else (metrics,)
-        )
-        self.target_layers: List[Union[nn.Module, torch.Tensor]] = []
+        if isinstance(metrics, list):
+            for m in metrics:
+                if not isinstance(m, GradientMetric):
+                    raise ValueError(
+                        "'metrics' needs to be a list of 'GradientMetric' instances. "
+                        f"Found {m.__class__.__name__}"
+                    )
+            if len(metrics) == 0:
+                raise ValueError("List of metrics is empty!")
+        elif not isinstance(metrics, GradientMetric):
+            raise ValueError(
+                "'metrics' needs to be either a 'GradientMetric' instance or "
+                "a list of 'GradientMetric' instances. "
+                f"Found '{type(metrics)}'"
+            )
 
-        # collect all parameters for zeroing gradients
-        t_layers = set()
-        for m in self.metrics:
-            t_layers.update(m.target_layers)
-        self.target_layers = list(t_layers)
+        self._metrics = [metrics] if isinstance(metrics, GradientMetric) else metrics
 
-        if len(self.metrics) == 0:
-            raise ValueError("No metrics specified!")
+        # Build mapping from parameters to gradient metrics to be able to map gradients
+        # to the correct metric.
+        # Also build a list of all parameters to compute the gradients on
+        _params_set = set()
+        self._param_metrics_map: Dict[
+            Union[torch.nn.parameter.Parameter, torch.Tensor], List[GradientMetric]
+        ] = defaultdict(list)
+        for metric in self._metrics:
+            _params_set.update(metric.target_parameters)
+            for param in metric.target_parameters:
+                self._param_metrics_map[param].append(metric)
+
+        self._params: List[torch.Tensor] = list(_params_set)
 
     def __call__(self, loss: torch.Tensor) -> torch.Tensor:
         """Computes gradient metrics per sample.
@@ -57,34 +75,28 @@ class GradientMetricCollector(object):
             raise ValueError(f"'loss' should have shape [N,] but found {loss.shape}")
 
         self.reset()
-        metrics = []
+        gradient_metrics = []
 
-        for sample_loss in loss:
-            sample_loss.backward(retain_graph=True)
+        for i, sample_loss in enumerate(loss):
+            gradients = torch.autograd.grad(
+                sample_loss,
+                self._params,
+                retain_graph=False if i == (len(loss) - 1) else True,
+            )
 
-            metrics.append(self.data)
+            for param, grad in zip(self._params, gradients):
+                for metric in self._param_metrics_map[param]:
+                    metric(grad)
+
+            gradient_metrics.append(self.data)
             self.reset()
-            self.zero_grad()
 
-        return torch.stack(metrics).to(loss.device)
+        return torch.stack(gradient_metrics).to(loss.device)
 
     def reset(self) -> None:
         """Resets all gradient metric instances to their default values."""
-        for m in self.metrics:
+        for m in self._metrics:
             m.reset()
-
-    def zero_grad(self) -> None:
-        for t in self.target_layers:
-            if isinstance(t, torch.Tensor):
-                # This part is taken from `torch.nn.Module.zero_grad`
-                if t.grad is not None:
-                    if t.grad.grad_fn is not None:
-                        t.grad.detach_()
-                    else:
-                        t.grad.requires_grad_(False)
-                    t.grad.zero_()
-            else:
-                t.zero_grad()
 
     @property
     def data(self) -> torch.Tensor:
@@ -97,7 +109,7 @@ class GradientMetricCollector(object):
                 concatenated. The output shape is ``(dim,)``.
         """
         metric_data = []
-        for m in self.metrics:
+        for m in self._metrics:
             metric_data.append(m.data)
 
         return torch.cat(metric_data)
